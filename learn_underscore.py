7eb42c55c032701a509d8b6df7f2cc97ded8e73a
ndyz_ Olivier <ndayizeyeolivier123@gmail.com>
2026-03-05 14:28:52 +0200

    Update learn_underscore.py

--- learn_underscore.py
+++ learn_underscore.py
@@ -3,20 +3,18 @@ class Person:
         self.name = name
         self.age = age
 
-    def display_info(self):
-        return f"Name: {self.name}, Age: {self.age}"
+    def displayinfo(self):
+        return "Name: " + self.name + ", Age: " + str(self.age)
 
 
 class Student(Person):
-    def __init__(self, name, age, grade):
-        super().__init__(name, age)
-        self.grade = grade
-
-    def display_info(self):
-        return f"{super().display_info()}, Grade: {self.grade}"
+    def displayinfo(self):
+        return Person.displayinfo(self) + ", Grade: " + self.grade
 
 
-student = Student("John", 15, "10th")
-print(student.display_info())
+student = Student("John", 15)  # uses Person __init__
+student.grade = "10th"         # set grade after
 
+print(student.displayinfo())
 
+# add 2 attributes that are not in human class # 
