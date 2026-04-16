0c167c31e01bd71e2065436ae12597a65c25f174
n2olivier <ndayizeyeolivier123@gmail.com>
2026-04-16 20:08:01 +0300

    Create olivierndaniel.py

---
+++ After Midterm/olivierndaniel.py
@@ -0,0 +1,25 @@
+class Daniel_k20250941_Olivier_K20250907(Exception):
+    pass
+
+
+class CMP142:
+    def __init__(self, attendance):
+        self.attendance = attendance
+
+    def check_quiz(self, num_students):
+        if num_students < 16:
+            print("No quiz, too few students in class.")
+
+        if num_students >= 16:
+            print("Get ready for quiz !")
+    
+c1 = CMP142(attendance=16)
+
+try:
+    attendance = float(input("Enter number of students: "))
+    c1.check_quiz(attendance)
+
+finally:
+    print("Thank you for attendance")
+
+# File for Daniel and Olivier!!
